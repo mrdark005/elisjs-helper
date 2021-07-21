59c0fef6a02c8e6a7a711971eb431acfcfb54dd6
MrDark <63722795+mrdark005@users.noreply.github.com>
2021-07-21 17:42:45 +0600

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,39 @@
+const fs = require("fs")
+
+class elisHelper {
+    constructor(client){
+        if(!client) throw new Error("client is not defined")
+        this.client = client
+    }
+    createHandler(options={folder:"commands", prefix:"!"}){
+        const prefix = options.prefix;
+        const client = this.client
+        const commandFiles = fs.readdirSync(options.folder).filter(file => file.endsWith(".js"));
+        
+        client.commands = new Map();
+        
+        for (const file of commandFiles) {
+            const command = require(`${options.folder}/${file}`);
+            client.commands.set(command.name, command);
+            console.log("command "+ command.name);
+        }
+        
+        client.events.messageCreate = async message => {
+            if (!message.content.startsWith(prefix) || message.author.bot) return;
+            const args = message.content.slice(prefix.length).trim().split(/ +/);
+            
+            const command = args.shift().toLowerCase();
+            if (!client.commands.has(command)) return;
+            try {
+                client.commands.get(command).execute(message, client, args);
+            } catch (error) {
+                console.error(error);
+            message.channel.send("Error!");
+        }
+    };
+ }
+}
+
+module.exports = {
+    client:elisHelper
+}
